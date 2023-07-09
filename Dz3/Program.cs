ab604f119582c4564f3742ed7b246cc97415e3ee
MargoStranger <margosebastian13@gmail.com>
2023-07-09 18:35:36 +0300

    сделано 3 дз

---
+++ Dz3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое* элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+// * Сумма всех цифр / их количество(это количество ваших строк)
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 11);
+PrintArray(array);
+
+Console.Write($"Среднее арифмитическоестолбцов  =  {String.Join("; ", GetMean(array))} ");
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] GetMean(int[,] inArray)
+{
+    double Sum = 0;
+    double[]  Mean = new double[inArray.GetLength(1)];
+    for (int i = 0; i < inArray.GetLength(1); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(0); j++)
+        {
+            Sum += inArray[j,i];
+        }
+        Mean[i] = Math.Round(Sum / inArray.GetLength(0), 2);
+        Sum = 0;
+    }
+    return Mean;
+}
